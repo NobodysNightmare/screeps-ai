d0d65dd18c25d5a3120744384294cf219b566b8a
Jan Sandbrink <fischikowski@web.de>
2017-01-04 23:07:48 +0100

    Some refactoring for claimGroup spawning

--- spawn.claimGroup.js
+++ spawn.claimGroup.js
@@ -9,11 +9,7 @@ var roles = {
 module.exports = {
     enqueue: function(spawn) {
         var target = Game.flags.Claim.pos;
-        spawn.room.memory.claimTarget = { 
-            x: target.x, 
-            y: target.y, 
-            roomName: target.roomName 
-        };
+        spawn.room.memory.claimTarget = this.serializePos(target);
         spawn.room.memory.claimQueue = [
             "claimer",
             "conqueror",
@@ -28,16 +24,34 @@ module.exports = {
         }
         
         var role = roles[nextRoleName];
+        if(this.spawnRole(spawn, role)) {
+            spawn.room.memory.claimQueue.shift();
+            if(spawn.room.memory.claimQueue.length == 0) {
+                spawn.room.memory.claimTarget = null;
+            }
+            return true;
+        }
+        
+        return false;
+    },
+    spawnRole: function(spawn, role, target) {
+        target = target || spawn.room.memory.claimTarget;
         var parts = _.find(role.partConfigs, function(config) {
             return _.sum(_.map(config, (part) => BODYPART_COST[part])) <= spawn.room.energyCapacityAvailable;
         });
 
         var result = spawn.createCreep(parts, undefined, { role: role.name, target: spawn.room.memory.claimTarget });
-        if(result == OK) {
-            spawn.room.memory.claimQueue.shift();
-            return true;
+        if(_.isString(result)) {
+            return result;
         }
         
         return false;
+    },
+    serializePos: function(target) {
+        return { 
+            x: target.x, 
+            y: target.y, 
+            roomName: target.roomName 
+        };
     }
 };
