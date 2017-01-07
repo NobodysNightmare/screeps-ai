8e065285a4ef09fcc02d40770a5a6282ac7f151d
Jan Sandbrink <nobodysnightmare@gmail.com>
2017-01-07 23:44:06 +0100

    Build extensions in two rings around spawn

--- construction.extensions.js
+++ construction.extensions.js
@@ -6,14 +6,16 @@ module.exports = {
         
         for(var spawn of room.find(FIND_MY_SPAWNS)) {
             var position = spawn.pos;
-            for(var xOff = -2; xOff <= 2; xOff += 2) {
-                for(var yOff = -2; yOff <= 2; yOff += 2) {
-                    if(xOff != 0 || yOff != 0) {
-                        var result = room.createConstructionSite(position.x + xOff,
-                                                                 position.y + yOff,
-                                                                 STRUCTURE_EXTENSION);
-                        if(result == ERR_RCL_NOT_ENOUGH || result == ERR_FULL) {
-                            return;
+            for(var distance = 2; distance <= 4; distance += 2) {
+                for(var xOff = -distance; xOff <= distance; xOff += 2) {
+                    for(var yOff = -distance; yOff <= distance; yOff += 2) {
+                        if(Math.abs(xOff) == distance || Math.abs(yOff) == distance) {
+                            var result = room.createConstructionSite(position.x + xOff,
+                                                                     position.y + yOff,
+                                                                     STRUCTURE_EXTENSION);
+                            if(result == ERR_RCL_NOT_ENOUGH || result == ERR_FULL) {
+                                return;
+                            }
                         }
                     }
                 }
