7022eee94ba450ab891c9c0b9e83a71137531dbe
Jan Sandbrink <fischikowski@web.de>
2017-01-29 14:48:04 +0100

    Upgraders take turns in withdrawing
    Only one withdraw per tick possible

--- role.upgrader.js
+++ role.upgrader.js
@@ -14,14 +14,22 @@ module.exports = {
         var controller = creep.room.controller;
         var container = logistic.storeFor(controller);
         if(container && container.store.energy > 0) {
-          var withdrawResult = creep.withdraw(container, RESOURCE_ENERGY);
-          if(withdrawResult == OK) {
-              if(creep.upgradeController(controller) == ERR_NOT_IN_RANGE) {
-                  creep.moveTo(controller);
-              }
-          } else if(withdrawResult == ERR_NOT_IN_RANGE) {
-              creep.moveTo(container);
-          }
+            var withdrawResult = OK;
+            
+            // only really withdraw when the carry is empty, because only one
+            // creep can withdraw from a container in the same tick. So we ensure
+            // that multiple ugraders can do their job simultaneously
+            if(creep.carry.energy == 0) {
+                withdrawResult = creep.withdraw(container, RESOURCE_ENERGY);
+            }
+            
+            if(withdrawResult == OK) {
+                if(creep.upgradeController(controller) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(controller);
+                }
+            } else if(withdrawResult == ERR_NOT_IN_RANGE) {
+                creep.moveTo(container);
+            }
           
           return;
         }
