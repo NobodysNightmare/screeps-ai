c66ee74d85a8908f1457ab42fa5b6cf8cdee34ee
Jan Sandbrink <nobodysnightmare@gmail.com>
2017-02-02 23:08:53 +0100

    Recycle defender after hostiles vanished

--- role.defender.js
+++ role.defender.js
@@ -7,8 +7,23 @@ module.exports = {
     ],
     run: function(creep) {
         var target = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        if(target) {
+            this.attack(creep, target);
+        } else {
+            this.recycle(creep);
+        }
+    },
+    attack: function(creep, target) {
         if(creep.attack(target) == ERR_NOT_IN_RANGE) {
             creep.moveTo(target);
+        }
+    },
+    recycle: function(creep) {
+        var spawn = creep.pos.findClosestByRange(FIND_MY_SPAWNS);
+        if(creep.pos.isNearTo(spawn)) {
+            spawn.recycleCreep(creep);
+        } else {
+            creep.moveTo(spawn);
         }
     }
 };
