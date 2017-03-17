76f183b23efcd9bf7d79a7211fdbafd3abbec6dc
Jan Sandbrink <nobodysnightmare@gmail.com>
2017-03-18 00:41:22 +0100

    Pass range to builders moveTo
    Avoid circumventing own walls, when they would be able to reach target
    ranged

--- role.builder.js
+++ role.builder.js
@@ -97,7 +97,7 @@ module.exports = {
             // lock onto target as soon as actual work is happening
             creep.memory.lastTarget = target.id;
         } else if(result == ERR_NOT_IN_RANGE) {
-            creep.moveTo(target);
+            creep.moveTo(target, { range: 3 });
         }
     },
     isConstructionSite: function(target) {
