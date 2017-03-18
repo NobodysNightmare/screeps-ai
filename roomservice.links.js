36660f469d8b5aa6129379ee13ef6e87d138cbeb
Jan Sandbrink <nobodysnightmare@gmail.com>
2017-03-18 02:16:01 +0100

    Also cache negative responses for links
    Otherwise the cache would be bypassed for all targets that do not have a
    link.
    We are now caching negative responses for 40 ticks.

--- roomservice.links.js
+++ roomservice.links.js
@@ -10,49 +10,55 @@ module.exports = class Links {
         }
         this.room = room;
     }
-    
+
     storage() {
         return this.linkAt(this.room.storage);
     }
-    
+
     controller() {
         return this.linkAt(this.room.controller);
     }
-    
+
     sources() {
         let sources = this.room.find(FIND_SOURCES);
         return _.compact(_.map(sources, (s) => this.linkAt(s)));
     }
-    
+
     linkAt(target) {
-        let link = Game.getObjectById(this.room.memory.links.cache[target.id]);
+        let linkId = this.room.memory.links.cache[target.id];
+        if(!linkId) return;
+        if(_.isNumber(linkId) && Game.time < linkId) return;
+
+        let link = Game.getObjectById(linkId);
         if(link) return link;
-        
+
         link = logistic.storeFor(target, false, STRUCTURE_LINK);
         if(link) {
             this.room.memory.links.cache[target.id] = link.id;
+        } else {
+            this.room.memory.links.cache[target.id] = Game.time + 40;
         }
-        
+
         return link;
     }
-    
+
     requestEnergy(link) {
         if(this.room.memory.links.requests.indexOf(link.id) === -1) {
            this.room.memory.links.requests.push(link.id);
         }
     }
-    
+
     cancelRequest(link) {
         let index = this.room.memory.links.requests.indexOf(link.id);
         if(index > -1) {
            this.room.memory.links.requests.splice(index, 1);
         }
     }
-    
+
     checkOpenRequests() {
         return this.room.memory.links.requests.length > 0;
     }
-    
+
     fullfillRequests() {
         if(this.storage().energy >= 500) {
             let target = Game.getObjectById(this.room.memory.links.requests.shift());
