88130dc4a5a3aee1d228b9611f14c1473525eddc
Jan Sandbrink <nobodysnightmare@gmail.com>
2017-06-27 22:19:41 +0200

    Improve and specialize attacker
    * now running on melee parts only
    * also accepting boosts
    * doing self defense while approaching target room

--- role.attacker.js
+++ role.attacker.js
@@ -1,3 +1,4 @@
+const boosting = require("helper.boosting");
 const ff = require("helper.friendFoeRecognition");
 const movement = require("helper.movement");
 
@@ -14,18 +15,24 @@ module.exports = {
         return configs;
     },
     run: function(creep) {
+        if(boosting.accept(creep, "XUH2O")) return;
+        
         var flag = Game.flags[creep.memory.flag];
         if(creep.pos.roomName == flag.pos.roomName) {
             this.attackRoom(creep);
         } else {
-            movement.moveToRoom(creep, flag.pos.roomName);
+            this.approachRoom(creep, flag.pos.roomName);
+        }
+    },
+    approachRoom: function(creep, roomName) {
+        movement.moveToRoom(creep, roomName);
+        let target = ff.findClosestHostileByRange(creep.pos);
+        if(target && creep.pos.isNearTo(target)) {
+            creep.attack(target);
         }
     },
     attackRoom: function(creep) {
         var target = null;
-        if(_.any(creep.body, (p) => p.type == RANGED_ATTACK)) {
-            target = ff.findClosestHostileByRange(creep.pos, { filter: (c) => creep.pos.inRangeTo(c, 3) });
-        }
 
         if(!target) {
             target = creep.pos.findClosestByRange(FIND_HOSTILE_STRUCTURES, { filter: (s) => s.structureType == STRUCTURE_TOWER });
@@ -50,9 +57,8 @@ module.exports = {
         }
     },
     attack: function(creep, target) {
-        let rangedResult = creep.rangedAttack(target);
-        let meleeResult = creep.attack(target);
-        if(rangedResult == ERR_NOT_IN_RANGE || meleeResult == ERR_NOT_IN_RANGE) {
+        let result = creep.attack(target);
+        if(result == ERR_NOT_IN_RANGE) {
             this.aggressiveMove(creep, target);
         }
     },
@@ -66,7 +72,6 @@ module.exports = {
             }
             
             if(moveTarget) {
-                creep.rangedAttack(moveTarget);
                 creep.attack(moveTarget);
             }
         }
