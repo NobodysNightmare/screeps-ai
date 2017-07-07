e3a6831ad74ec9e348afd2b58e4ab0e5c56945a7
Jan Sandbrink <nobodysnightmare@gmail.com>
2017-07-08 00:54:08 +0200

    Reprioritize tower targeting
    Optimizing for combination with guards

--- structure.tower.js
+++ structure.tower.js
@@ -2,19 +2,27 @@ const ff = require("helper.friendFoeRecognition");
 
 module.exports = {
     run: function(tower) {
-        var closestHostile = ff.findClosestHostileByRange(tower.pos);
-        if(closestHostile) {
-            tower.attack(closestHostile);
+        let friends = tower.room.find(FIND_MY_CREEPS, { filter: (creep) => creep.hits < creep.hitsMax && creep.memory.role !== "hopper" });
+        let warriors = _.filter(friends, (f) => _.some(f.body, (p) => p.type === ATTACK || p.type === RANGED_ATTACK));
+        
+        if(warriors.length > 0) {
+            tower.heal(_.sortBy(warriors, (w) => w.pos.getRangeTo(tower))[0]);
             return;
         }
         
-        var closestFriendly = tower.pos.findClosestByRange(FIND_MY_CREEPS, { filter: (creep) => creep.hits < creep.hitsMax && creep.memory.role !== "hopper" });
-        if(closestFriendly) {
-            tower.heal(closestFriendly);
+        let hostiles = _.sortBy(ff.findHostiles(tower.room), (h) => h.hits - h.hitsMax);
+        if(hostiles.length > 0) {
+            tower.attack(hostiles[0]);
             return;
         }
         
-        var closestDamagedStructure = tower.pos.findClosestByRange(FIND_MY_STRUCTURES, { filter: (s) => s.hits < s.hitsMax && s.hits < 5000 });
+        
+        if(friends.length > 0) {
+            tower.heal(_.sortBy(friends, (f) => f.pos.getRangeTo(tower))[0]);
+            return;
+        }
+        
+        let closestDamagedStructure = tower.pos.findClosestByRange(FIND_MY_STRUCTURES, { filter: (s) => s.hits < s.hitsMax && s.hits < 5000 });
         if(closestDamagedStructure) {
             tower.repair(closestDamagedStructure);
         }
