5cba1b9aed30b14c44f32a29298dc51ad2a0d3c4
Jan Sandbrink <nobodysnightmare@gmail.com>
2017-10-03 22:23:56 +0200

    Improve power farming
    Farmers will make way for scoopers on tight collection spots.

--- role.powerFarmer.js
+++ role.powerFarmer.js
@@ -13,8 +13,13 @@ module.exports = {
         let target = creep.pos.findClosestByRange(FIND_STRUCTURES,
             { filter: (s) => s.structureType == STRUCTURE_POWER_BANK });
 
-        if(!target) return;
-
+        if(target) {
+            this.attackBank(creep, target);
+        } else {
+            this.clearPath(creep);
+        }
+    },
+    attackBank: function(creep, target) {
         let returnDamage = POWER_BANK_HIT_BACK * ATTACK_POWER * creep.getActiveBodyparts(ATTACK);
 
         if(returnDamage >= creep.hits) return;
@@ -22,6 +27,11 @@ module.exports = {
         if(creep.attack(target) == ERR_NOT_IN_RANGE) {
             creep.moveTo(target);
         }
+    },
+    clearPath: function(creep) {
+        let resources = creep.room.find(FIND_DROPPED_RESOURCES);
+        if(resources.length === 0) return;
+        creep.fleeFrom(resources, 4);
     }
 };
 
