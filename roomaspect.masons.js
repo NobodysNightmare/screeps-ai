fc45e7fa78d31870a10852e82eb4b8f3f4d141d7
Jan Sandbrink <nobodysnightmare@gmail.com>
2018-04-07 12:55:12 +0200

    Also spawn masons in response to nukes

--- roomaspect.masons.js
+++ roomaspect.masons.js
@@ -15,10 +15,19 @@ module.exports = class MasonsAspect {
         let parts = spawnHelper.bestAvailableParts(this.room, mason.configs(16));
         this.roomai.spawn(parts, { role: mason.name, room: this.room.name });
     }
-
+    
     masonCount() {
         if(!this.room.storage) return 0;
+        
+        return Math.max(this.neededForNukes(), this.neededForWalls());
+    }
+    
+    neededForNukes() {
+        let nukes = this.room.find(FIND_NUKES);
+        return Math.min(nukes.length, 2);
+    }
 
+    neededForWalls() {
         if(this.room.storage.store.energy < 200000) {
             return 0;
         } else if(this.room.storage.store.energy < 300000) {
