e59143c68260de822e36634975e1d5c418f53185
Jan Sandbrink <nobodysnightmare@gmail.com>
2018-07-05 08:00:54 +0200

    Protect terminals from overflowing
    * Don't export minerals to them, if they have no energy to transfer
      the minerals away
    * If the terminal got overfilled, import excess resources from it
        -> in a super bad room situation this may lead to the storage getting
           overfilled subsequently, which would then trigger its own
           safety valve
        -> nominal storages should be able to take in a complete terminal
           before reaching overflow

--- role.trader.js
+++ role.trader.js
@@ -1,27 +1,57 @@
 const spawnHelper = require("helper.spawning");
 
+// exporting anything does not make sense with less than
+// this amount of energy in the terminal
+const EXPORT_ENERGY_THRESHOLD = 25000;
+
+const STORAGE_ENERGY_RESERVE = 100000;
+
+// if free space in the terminal gets below this value, we consider it overfilled
+const TERMINAL_WORKING_BUFFER = 1000;
+
 module.exports = {
     name: "trader",
     parts: spawnHelper.makeParts(10, CARRY, 5, MOVE),
     run: function(creep) {
         let trading = creep.room.ai().trading;
+        let terminal = creep.room.terminal;
+        let storage = creep.room.storage;
         if(creep.memory.exporting) {
-            if(!this.carryTo(creep, creep.room.terminal)) {
-                let resource = _.last(trading.resourcesImportableToStorage);
-                if(resource) {
-                    let amount = Math.min(creep.carryCapacity, creep.room.terminal.store[resource], trading.neededImportToStorage(resource));
-                    creep.withdraw(creep.room.terminal, resource, amount);
-                }
+            if(!this.carryTo(creep, terminal)) {
                 creep.memory.exporting = false;
+
+                let importResource = _.last(trading.resourcesImportableToStorage);
+                if(importResource) {
+                    let amount = Math.min(creep.carryCapacity, terminal.store[importResource], trading.neededImportToStorage(importResource));
+                    creep.withdraw(terminal, importResource, amount);
+                } else if(terminal.storeCapacity - _.sum(terminal.store) < TERMINAL_WORKING_BUFFER) {
+                    let excessResource = _.invert(terminal.store)[_.sortBy(terminal.store, (r) => -r)[0]];
+                    console.log("Terminal in room " + creep.room.name + " is overfilled! Temporarily importing " + excessResource);
+                    creep.withdraw(terminal, excessResource);
+                }
             }
         } else {
-            if(!this.carryTo(creep, creep.room.storage)) {
-                let resource = _.last(trading.resourcesExportableFromStorage);
-                if(resource) {
-                    let amount = Math.min(creep.carryCapacity, trading.possibleExportFromStorage(resource));
-                    creep.withdraw(creep.room.storage, resource, amount);
-                }
+            if(!this.carryTo(creep, storage)) {
                 creep.memory.exporting = true;
+
+                let exportResource = _.last(trading.resourcesExportableFromStorage);
+                if(exportResource) {
+                    let amount = Math.min(creep.carryCapacity, trading.possibleExportFromStorage(exportResource));
+
+                    // overflow protection for terminal
+                    // do not put further minerals in, if there is no energy to export them
+                    if(terminal.store.energy < EXPORT_ENERGY_THRESHOLD) {
+                        let terminalCapacity = terminal.storeCapacity - _.sum(terminal.store);
+                        if(storage.store.enery > STORAGE_ENERGY_RESERVE && terminalCapacity > 0) {
+                            exportResource = RESOURCE_ENERGY;
+                            amount = Math.min(creep.carryCapacity, terminalCapacity);
+                        } else {
+                            return;
+                        }
+                    }
+
+                    creep.withdraw(storage, exportResource, amount);
+                }
             }
         }
     },
