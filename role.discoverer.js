427c34f632c72e24dcec820bf4236762c339772d
Jan Sandbrink <nobodysnightmare@gmail.com>
2020-10-29 07:52:33 +0100

    Check off discovered rooms earlier
    Previously a room was only removed from the
    target list, if it was the current main target
    of the discoverer.
    
    Though path finding often caused the discoverer to
    pass through rooms later on the list first.
    
    Now all rooms visited by the discoverer are considered done,
    regardless of their list position.

--- role.discoverer.js
+++ role.discoverer.js
@@ -5,14 +5,12 @@ module.exports = {
     parts: [MOVE],
     run: function(creep) {
         if(creep.ticksToLive == CREEP_LIFE_TIME - 1) creep.notifyWhenAttacked(false);
-        
-        if(creep.room.name === creep.memory.targets[0]) {
-            if(creep.memory.targets.length > 1) {
-                creep.memory.targets.shift();
-            }
-        }
 
-        movement.moveToRoom(creep, creep.memory.targets[0]);
+        creep.memory.targets = _.filter(creep.memory.targets, (r) => r !== creep.room.name);
+
+        if(creep.memory.targets.length > 1) {
+            movement.moveToRoom(creep, creep.memory.targets[0]);
+        }
     }
 };
 
