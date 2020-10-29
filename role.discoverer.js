6433c485441eefd897d97aa56e12926ee248860a
Jan Sandbrink <nobodysnightmare@gmail.com>
2020-10-29 07:45:18 +0100

    Do not notify when discoverers are attacked

--- role.discoverer.js
+++ role.discoverer.js
@@ -4,12 +4,14 @@ module.exports = {
     name: "discoverer",
     parts: [MOVE],
     run: function(creep) {
+        if(creep.ticksToLive == CREEP_LIFE_TIME - 1) creep.notifyWhenAttacked(false);
+        
         if(creep.room.name === creep.memory.targets[0]) {
             if(creep.memory.targets.length > 1) {
                 creep.memory.targets.shift();
             }
         }
-        
+
         movement.moveToRoom(creep, creep.memory.targets[0]);
     }
 };
